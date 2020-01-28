90a75722cb62bb18733db628dea105803aa62083
Bill Vasilopoulos <billvasilopoulos@Bills-MBP-2.domain>
2020-01-27 20:43:49 -0800

    added component class for scoreClass with overlay

---
+++ public/Components.js
@@ -0,0 +1,20 @@
+export const score = (score, animClass) =>
+  `<span class="game__score ${animClass}">${score}</span>`;
+
+export const gameOverContent = score => {
+  const scoreClass = score > 0 ? 'plus' : 'minus';
+
+  return `
+    <div class="summary__wrapper">
+      <h2 class="summary__title">Game Over!</h2>
+      <p class="summary__subtitle">You got</p>
+      <p class="summary__value ${scoreClass}">${score} <small>points</small></p>
+      <a href="/" class="button button--primary button--block button--lg">
+        Play Again
+      </a>
+    </div>
+  `;
+};
+
+export const overlayContent = content =>
+  `<span class="overlay__text">${content}</span>`;
