91b9766b3ff84a89dc25c85cb5d2311b13ba5481
Bill Vasilopoulos <billvasilopoulos@Bills-MBP-2.domain>
2020-01-30 12:56:58 -0800

    added redirect HTTP traffic to HTTPS func to express server

--- server.js
+++ server.js
@@ -23,6 +23,21 @@ app.get('/', function(request, response) {
   response.sendFile(`${__dirname}/views/index.html`);
 });
 
+app.enable('trust proxy');
+
+// Add a handler to inspect the req.secure flag (see
+// http://expressjs.com/api#req.secure). This allows us
+// to know whether the request was via http or https.
+app.use(function(req, res, next) {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
   console.log(`Your app is listening on port ${listener.address().port}`);
